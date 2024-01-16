d6bb2e0d8026837eb0ba3142f46297e4d4f7e201
nourmidan <nourmidan67@gmail.com>
2024-01-17 01:07:39 +0200

    feat: first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,213 @@
-import Image from 'next/image'
+"use client";
+import React, { useEffect, useState } from "react";
+import html2canvas from "html2canvas";
+// import headerBg from '/assets/header_bg.png'
+// import slide from '/assets/slide.jpg'
+// import logo from '/assets/logo.png'
+// import logo3 from '/assets/logo3.png'
+import Image from "next/image";
+const App = () => {
+  const [password, setPassword] = useState("");
+  const [baladyPrice, setBaladyPrice] = useState();
+  const [whitePrice, setWhitePrice] = useState();
+  const [redPrice, setRedPrice] = useState();
+  const [date, setDate] = useState();
+  const handlePasswordSubmit = () => {
+    if (password === "your_password") {
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  };
 
-export default function Home() {
+  const handleDownloadImage = async () => {
+    html2canvas(document.querySelector("#uiContainer"),{scale:.9}).then((canvas) => {
+      let link = document.createElement("a");
+      link.href = canvas.toDataURL();
+      link.download = "ui_design.png";
+      link.click();
+    });
+  };
+  const currentDate = new Date();
+
+  const formattedDate = new Intl.DateTimeFormat("ar", {
+    day: "2-digit", // Display day as two digits
+    weekday: "long", // Display full weekday name
+    year: "numeric",
+    month: "2-digit", // Display month as two digits
+  }).format(currentDate);
+
+  useEffect(() => {
+    setDate(formattedDate);
+  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className=" " style={{    overflow:'visible !important'    }}>
+      {/* Password Prompt */}
+      {/* {password !== 'your_password' && (
+        <div className="my-4">
+          <label className="mr-2">
+            Enter Password:
+            <input
+              className="border border-gray-300 rounded px-2 py-1"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-          </a>
+          </label>
+          <button
+            className="bg-blue-500 text-white px-4 py-1 rounded cursor-pointer"
+            onClick={handlePasswordSubmit}
+          >
+            Submit
+          </button>
         </div>
-      </div>
+      )} */}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* UI */}
+      {password === "your_password" ||
+        (true && (
+          <div
+            id="uiContainer"
+            className="text-center w-[900px] h-[1070px]   relative "
+            style={{
+              backgroundImage: `url(/slide.jpg)`,
+              backgroundSize: "auto 350px",
+              objectFit: "",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <img src={"/header_bg.png"} width="900" height="100" />
+            <h1 className="text-lg absolute top-0 w-full text-white font-bold mb-2">
+              الحَمدُ لِلَّهِ الَّذي أَنزَلَ عَلى عَبدِهِ الكِتابَ وَلَم يَجعَل
+              لَهُ عِوَجًا
+            </h1>
+            <div className="flex h-24">
+              <img src={"/logo3.png"} className="ml-2" />
+              <img src={"/logo.png"} className="ml-auto mr-2" />
+            </div>
+            <div className="flex justify-center py-10 h-96 absolute top-0 w-full">
+              <img src={"/slide3.jpg"} className="  bg-black" />
+            </div>
+            <div className="absolute top-[343px]">
+              <div className="relative">
+                <img src={"/over_slider.png"} className="ml-auto mr-2" />
+                <div className="flex justify-center items-center">
+                  <h1 className="absolute top-0 text-lg text-[#0d579e] font-bold ">
+                    رَبَّنَـا لَا تُؤَاخِذْنَـا إِن نَّسِـينَا أَوْ
+                    أَخْـطَأْنَـا
+                  </h1>
+                  <div className="mt-5">
+                    <h1 className="font-['arabic'] text-[#0d579e] text-2xl mb-2">
+                      متوسط تداول أسعار مبيعات أمس
+                    </h1>
+                    <h1 className="text-[#0d579e] font-['arabic'] text-2xl">
+                      لجمله بيض المائده الصحراوي والزراعي
+                    </h1>
+                    <div className="bg-[#F59933] w-12 h-1 ml-auto mr-auto mt-2"></div>
+                    <h1 className="text-[#0D579E] font['arabic'] text-3xl font-bold mt-3">
+                      01222206011{" "}
+                      <span className="text-[#F59933] font-normal">
+                        // {""}
+                      </span>
+                      01003731392
+                    </h1>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+                    <div className="flex justify-center mb-5">
+                      <div className="w-[550px] h-10 bg-[#0D579E] rounded mt-4 font-['arabic'] text-xl text-white flex   justify-center">
+                        {date}
+                      </div>
+                    </div>
+
+                    <div className=" flex justify-center gap-6 mb-16">
+                  
+                    <div >
+                        <div 
+                          className="relative "
+                          >
+                        <img
+                          src={"/eggs-white.png"}
+                          width={250}
+                          />
+                          <div className="absolute -bottom-7 w-full flex justify-center">
+                        <div className="w-24 h-14 rounded-xl border border-[#48A6FF] bg-white font-[arabic] flex  justify-center text-2xl ">{whitePrice || '00.00'}</div>
+                            </div>
+                          </div>
+                      </div>
+                      <div >
+                        <div 
+                          className="relative "
+                          >
+                        <img
+                          src={"/eggs-red.png"}
+                          width={250}
+                          />
+                          <div className="absolute -bottom-7 w-full flex justify-center">
+                        <div className="w-24 h-14 rounded-xl border border-[#48A6FF] bg-white font-[arabic] flex  justify-center text-2xl ">{redPrice || '00.00'}</div>
+                            </div>
+                          </div>
+                      </div>
+
+                    
+
+
+                      <div >
+                        <div 
+                          className="relative "
+                          >
+                        <img
+                          src={"/eggs-balady.png"}
+                          width={250}
+                          />
+                          <div className="absolute -bottom-7 w-full flex justify-center">
+                        <div className="w-24 h-14 rounded-xl border border-[#48A6FF] bg-white font-[arabic] flex  justify-center text-2xl ">{baladyPrice || '00.00'}</div>
+                            </div>
+                          </div>
+                          
+                      </div>
+
+                    </div>
+                      <img src={"/footer.png"} width="900" height="100" />
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+
+        
+          </div>
+        ))}
+          
+          <div className="flex flex-col w-1/3">
+
+          <input
+            className="border border-gray-300 rounded px-2 py-1  mb-4"
+            type="text"
+            id="editableText"
+            value={redPrice}
+            onChange={(e) => setRedPrice(e.target.value)}
+            />
+            <input
+            className="border border-gray-300 rounded px-2 py-1  mb-4"
+            type="text"
+            id="editableText"
+            value={whitePrice}
+            onChange={(e) => setWhitePrice(e.target.value)}
+            />
+            <input
+            className="border border-gray-300 rounded px-2 py-1  mb-4"
+            type="text"
+            id="editableText"
+            value={baladyPrice}
+            onChange={(e) => setBaladyPrice(e.target.value)}
+            />
+      <button
+        className="bg-green-500 text-white px-4 py-2 rounded cursor-pointer "
+        onClick={handleDownloadImage}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        Download Image
+      </button>
+        </div>
+    </div>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default App;
